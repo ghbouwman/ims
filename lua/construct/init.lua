fe2a4cec044519c3231e1d3d19d03bcd3265a597
Hans Bouwman <hans.bouwman@protonmail.com>
2024-11-27 16:02:37 +0100

    rename

--- lua/construct/init.lua
+++
@@ -1,8 +0,0 @@
-create = create or {}
-
-require("api-wrapper")
-require("rod")
-require("octupole")
-require("diaphragm")
-
-return create
---
+++ lua/create/init.lua
@@ -0,0 +1,8 @@
+create = create or {}
+
+require("create.electrode")
+require("create.rod")
+require("create.octupole")
+require("create.diaphragm")
+
+return create
