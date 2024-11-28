7413cf20d3fd73805118518b3ebb1684848659d0
Hans Bouwman <hans.bouwman@protonmail.com>
2024-11-28 11:57:32 +0100

    improved data format

--- src/setups/ims.lua
+++ src/setups/ims.lua
@@ -2,30 +2,37 @@
 
 setups = setups or {}
 
+-- explanation:
+-- a setup is made up of components, each component has two fields:
+-- the geometry (including positioning) and the electrical properties
+-- all geometry units are in milimeters; all electrical properties in SI (V, Hz, s, etc.)
+
 setups.ims = 
 {
         inlet = 
         {
-                kind = "diaphragm",
                 geometry =
                 {
+                        kind            = "diaphragm",
                         thickness       = 2,
                         begin_diameter  = 2,
                         end_diameter    = 6,
-                        z_begin         = 0,     -- distance from origin
+                        z_begin         = 0,    -- distance from origin
                         z_end           = 0 + 2 -- distance from origin
         
                 },
-                voltage = 0     -- Volts
+                electrical =
+                {
+                        kind    = "static",
+                        voltage = 0     
+                }
 
         },
-        
-        
         octupole =
         {
-                kind = "octupole",
                 geometry =
                 {
+                        kind                    = "octupole",
                         length                  = 250,          
                         rod_diameter            = 5.0,          
                         inscribed_diameter      = 13.44,        
@@ -35,28 +42,33 @@ setups.ims =
                         rod_centers_diameter    = 13.44 + 5.0,     
                         rod_centers_radius      = (13.44 + 5.0) / 2
                 },
+                electrical =
+                {
+                        kind            = "sinusoidal",
+                        Vpp             = 350,          
+                        freq            = 700_000,      
+                        offset          = 4,            
+                        Vamplitude      = 350 / 2,      
+                        phase           = 0             
+                }
         
-                -- electronics
-                Vpp             = 350,          -- V
-                freq            = 700,          -- kHz
-                offset          = 4,            -- V
-                Vamplitude      = 350 / 2       -- V
         },
-        
         outlet = 
         {
-                kind = "diaphragm",
                 geometry =
                 {
+                        kind            = "diaphragm",
                         thickness       = 2,
                         begin_diameter  = 0.6,
                         end_diameter    = 0.6,  -- probably not the actual value, but that doesn't matter
                         z_begin         = 0 + 2 + 2 + 2 + 250 + 1,      -- distance from origin
                         z_end           = 0 + 2 + 2 + 2 + 250 + 1 + 1   -- distance from origin 
                 },
-
-                -- electronics
-                voltage = -30   -- V
+                electrical =
+                {
+                        kind    = "static",
+                        voltage = -30  
+                }
         }
 }        
 
